6e1f9836d3e16e908ebea8479df34a75c9bd4230
shovalf <58768407+shovalf@users.noreply.github.com>
2021-01-29 18:18:08 +0200

    Update QGCN.py

--- QGCN_model/QGCN.py
+++ QGCN_model/QGCN.py
@@ -31,7 +31,7 @@ class GCN(Module):
         self._linear = Linear(in_dim, out_dim)
         self._activation = activation
         self._dropout = Dropout(p=dropout)
-        self._gpu = False
+        self._gpu = False  # turn to True if you have access to gpu
 
     def forward(self, A, x0):
         # Dropout layer
@@ -39,14 +39,11 @@ class GCN(Module):
         # tanh( A(n x n) * x0(n x d) * W1(d x k) )
         Ax = torch.matmul(A, x0)
 
-        x = torch.log(torch.abs(self._linear(Ax)) + 1e-4)
-        x1 = self._activation(x)
+        x = self._linear(Ax)
+        x1 = self._activation(x) - 2/(x**2+1)
+        
         return x1
-
-    @ staticmethod
-    def return_ax(A, x0):
-        return torch.matmul(A, x0)
-
+    
 
 # class of last layer of the QGCN
 class QGCNLastLayer(Module):
@@ -58,7 +55,6 @@ class QGCNLastLayer(Module):
         self._gpu = False
 
     def forward(self, A, x0, x1):
-        # sigmoid( W2(1 x k) * trans(x1)(k x n) * A(n x n) * x0(n x d) * W1(d x 1) )
         x1_A = torch.matmul(x1.permute(0, 2, 1), A)
         W2_x1_A = self._left_linear(x1_A.permute(0, 2, 1))
         W2_x1_A_x0 = torch.matmul(W2_x1_A.permute(0, 2, 1), x0)
@@ -94,6 +90,10 @@ class QGCN(Module):
             for vocab_dim, embed_dim in zip(embed_vocab_dim, self._params["embeddings_dim"]):
                 self._embed_layers.append(Embedding(vocab_dim, embed_dim))
             self._embed_layers = ModuleList(self._embed_layers)
+        
+        values_out = [qgcn_layers_dim[i]["out"] for i in range(len(qgcn_layers_dim))]
+        values_out.append(qgcn_layers_dim[0]["in"])
+        sum_layers_shape = sum(values_out)
 
         self._num_layers = len(self._params["GCN_layers"])
         self._linear_layers = []
@@ -101,9 +101,24 @@ class QGCN(Module):
         self._linear_layers = ModuleList([GCN(qgcn_layers_dim[i]["in"], qgcn_layers_dim[i]["out"],
                                               globals()[self._params["activation"]], self._params["dropout"])
                                           for i in range(self._num_layers)])
-        qgcn_right_in = in_dim + sum(self._params["embeddings_dim"])
-        self._qgcn_last_layer = QGCNLastLayer(left_in_dim=qgcn_layers_dim[-1]["out"], right_in_dim=qgcn_right_in,
-                                              out_dim=1 if self._is_binary else self._params["num_classes"])
+        
+        # W2_x1_A_x0_W3 where x1 is the last layer of GCN and x0 is the input layer (what we have done in our experiments)
+        if self._params["f"] == "x1_x0":
+            qgcn_right_in = in_dim + sum(self._params["embeddings_dim"])
+            qgcn_left_in = qgcn_layers_dim[-1]["out"]
+        # W2_x1_A_x1_W3 where x1 is the last layer of GCN
+        elif self._params["f"] == "x1_x1":
+            qgcn_right_in = qgcn_layers_dim[-1]["out"]
+            qgcn_left_in = qgcn_layers_dim[-1]["out"]
+        # W2_c_A_x0_W3 where c is the concatanation of all layers of GCN andx1 is the last layer of GCN
+        elif self._params["f"] == "c_x1":
+            qgcn_right_in = qgcn_layers_dim[-1]["out"]
+            qgcn_left_in = sum_layers_shape
+        # if invalid value is inserted, do the regular version - "x1_x0"
+        else:
+            qgcn_right_in = in_dim + sum(self._params["embeddings_dim"])
+            qgcn_left_in = qgcn_layers_dim[-1]["out"]
+            
         self._softmax = LogSoftmax(dim=1)
         self.optimizer = self.set_optimizer(self._params["lr"], globals()[self._params["optimizer"]], self._params["L2_regularization"])
 
@@ -121,9 +136,21 @@ class QGCN(Module):
             x0 = torch.cat([x0] + list_embed, dim=2)
 
         x1 = x0
+        H_layers = [x1]
         for i in range(self._num_layers):
             x1 = self._linear_layers[i](A, x1)
+            H_layers.append(x1)
 
-        x2 = self._qgcn_last_layer(A, x0, x1)
+        c = torch.cat(H_layers, dim=2)
+
+        if self._params["f"] == "x1_x0":
+            x2 = self._qgcn_last_layer(A, x0, x1)
+        elif self._params["f"] == "x1_x1":
+            x2 = self._qgcn_last_layer(A, x1, x1)
+        elif self._params["f"] == "c_x1":
+            x2 = self._qgcn_last_layer(A, x1, c)
+        else: # like "x1_x0"
+            x2 = self._qgcn_last_layer(A, x0, x1)
+
         x2 = torch.sigmoid(x2) if self._is_binary else self._softmax(x2)
         return x2
